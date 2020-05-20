6869393bf62ea469533686bcd685117ec1c7f1e1
Shani <shanishalel89@gmail.com>
2020-05-20 19:14:04 +0300

    make some changes

--- Board.cpp
+++ Board.cpp
@@ -1,11 +1,12 @@
 #include "Board.hpp"
+#include <iostream>
+using namespace std;
+
 namespace WarGame {
 
 
          Soldier*& Board :: operator[](std::pair<int,int> location){
-
-             board[location.first][location.second] = this;
-             return *board;
+             return board[location.first][location.second];
         }
 
 
@@ -13,12 +14,27 @@ namespace WarGame {
             return  board[location.first][location.second];
         }
 
-        void move(uint player_number, std::pair<int,int> source, MoveDIR direction) {
+        void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction) {
 
         }
 
-         bool has_soldiers(uint player_number) {
+    //needs to check
+         bool Board::has_soldiers(uint player_number) const {
+            int count=0;
+             for(int i=0; i<board.size();i++){
+                 for(int j=0; j<board[i].size();j++){
+                     Soldier* s= board[i][j];
+                     if(s!=nullptr){
+                         count++;
+                     }
+                 }
 
+             }
+             if(count>player_number){
+                 return true;
+             }else{
+             return false;
+             }
 
          }
 }
